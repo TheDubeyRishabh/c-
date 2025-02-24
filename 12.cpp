d3eaeb4e8713eb13b5a8733ddf2d581cd4304ab5
TheDubeyRishabh <therishabhdube@gmail.com>
2025-02-24 17:46:53 +0000

    c++

---
+++ 12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Debugger!" << endl;  // Ensure some output
+    int x = 10;  // Add a variable
+    cout<<10;
+    
+    return 0;
+}
